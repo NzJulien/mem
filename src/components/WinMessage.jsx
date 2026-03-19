abe06d56eaf32f1458fe5085fae88aa5646f9bb9
Nzoko Julien <nzokojulien0@gmail.com>
2026-03-19 02:15:24 +0100

    update

---
+++ src/components/WinMessage.jsx
@@ -0,0 +1,12 @@
+function WinMessage() {
+
+    return (
+        <div className="win-messsage">
+            <h2>
+                congratulations
+            </h2>
+            <p>You completed the game in {moves} moves!</p>
+        </div>
+    )
+}
+export default WinMessage
